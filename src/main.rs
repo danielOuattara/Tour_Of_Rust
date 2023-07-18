e67cd8d2c13b5a0f4db1f42611a0891b881aaad3
danielOuattara <daniel.ouattara@gmx.com>
2023-07-18 21:46:35 +0200

    chapiter 1 : Tour of Rust

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod chapiter_1;
+
+fn main() {
+    println!("Hello, world!");
+
+    chapiter_1::variables();
+    chapiter_1::changing_variables();
+    chapiter_1::basic_types();
+    chapiter_1::basic_conversion();
+    chapiter_1::constants();
+    chapiter_1::arrays();
+    chapiter_1::functions_1();
+    chapiter_1::functions_2();
+    chapiter_1::functions_3();
+}
