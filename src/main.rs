059163f6178d2b840a6457d4f056adcaa3c48cc9
danielOuattara <daniel.ouattara@gmx.com>
2023-07-26 21:02:59 +0200

    main updated

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
-mod chapiter_1;
+// mod _01_basics;
+// mod _02_basics_flow_control;
+// mod _03_basic_data_structure_types;
+// mod _04_generics_types;
 
 fn main() {
-    println!("Hello, world!");
+    // _01_basics::variables();
+    // _01_basics::changing_variables();
+    // _01_basics::basic_types();
+    // _01_basics::basic_conversion();
+    // _01_basics::constants();
+    // _01_basics::arrays();
+    // _01_basics::functions_1();
+    // _01_basics::functions_2();
+    // _01_basics::functions_3();
 
-    chapiter_1::variables();
-    chapiter_1::changing_variables();
-    chapiter_1::basic_types();
-    chapiter_1::basic_conversion();
-    chapiter_1::constants();
-    chapiter_1::arrays();
-    chapiter_1::functions_1();
-    chapiter_1::functions_2();
-    chapiter_1::functions_3();
+    //------------------------------
+
+    // _02_basics_flow_control::if_else();
+    // _02_basics_flow_control::loop_loop();
+    // _02_basics_flow_control::while_loop();
+    // _02_basics_flow_control::for_loop();
+    // _02_basics_flow_control::match_tool();
+    // _02_basics_flow_control::return_values_from_loops();
+    // _02_basics_flow_control::return_values_from_block_expressions();
+
+    //------------------------------
+
+    // _03_basic_data_structure_types::structures();
+    // _03_basic_data_structure_types::calling_methods();
+    // _03_basic_data_structure_types::memory();
+    // _03_basic_data_structure_types::structures();
+    // _03_basic_data_structure_types::tuple_like_structs();
+    // _03_basic_data_structure_types::unit_like_structs();
+    // _03_basic_data_structure_types::enumeration();
+    // _03_basic_data_structure_types::enumeration();
+
+    //-----------------------------
+
+    // _04_generics_types::what_are_generics_types();
 }
