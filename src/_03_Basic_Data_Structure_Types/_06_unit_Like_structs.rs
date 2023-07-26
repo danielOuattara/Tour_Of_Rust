199ea72f121d7f9eac6fb79cd6c4135d2ccf537c
danielOuattara <daniel.ouattara@gmx.com>
2023-07-26 21:13:31 +0200

    03 Basic Data Structure:06 unit like Struct

---
+++ src/_03_Basic_Data_Structure_Types/_06_unit_Like_structs.rs
@@ -0,0 +1,18 @@
+/*
+Unit-Like Structs
+==================
+
+Structs do not have to have any fields at all.
+
+As mentioned in Chapter 1 a unit is another word
+for an empty tuple (). This is why this kind of
+struct is called Unit-like.
+
+This type of struct is rarely used.
+*/
+
+struct Marker;
+
+fn main() {
+    let _m = Marker;
+}
