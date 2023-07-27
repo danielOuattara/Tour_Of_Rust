9e07418318d6f9299ba65685bf40d9ada6f0203e
danielOuattara <daniel.ouattara@gmx.com>
2023-07-27 23:22:50 +0200

    05 OwnerShip: 05 returning  ownership

---
+++ src/_05_Ownership_And_Borrowing_Data/_05_returning_ownership.rs
@@ -0,0 +1,28 @@
+/* 
+Returning Ownership
+=====================
+
+Ownership can also be returned from a function.
+
+*/
+
+struct Foo {
+    x: i32,
+}
+
+fn do_something() -> Foo {
+    Foo { x: 42 }
+    // ownership is moved out
+}
+
+fn main() {
+    let foo = do_something();
+    // foo becomes the owner
+
+    println!("{}", foo.x);
+
+    
+    // End Of Scope
+    // foo is dropped because of end of function scope
+
+}
