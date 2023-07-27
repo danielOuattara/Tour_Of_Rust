e8d2b70f71d6f1784aa6f3c3f05475ec00fa2f7e
danielOuattara <daniel.ouattara@gmx.com>
2023-07-27 23:23:46 +0200

    05 OwnerShip: 06 borrowing  ownership  with ref &

---
+++ src/_05_Ownership_And_Borrowing_Data/_06_borrowing_ownership_with_references.rs
@@ -0,0 +1,27 @@
+/*
+Borrowing Ownership with References
+====================================
+
+References allow us borrow access to a resource 
+with the '&' operator.
+
+References are also dropped like other resources.
+
+*/
+
+struct Foo {
+    x: i32,
+}
+
+fn main() {
+    let foo = Foo { x: 42 };
+    
+    let f = &foo;
+    println!("{}", f.x);
+    
+    println!("{}", foo.x);
+
+    // End Of Scope:
+    // f is dropped here
+    // foo is dropped here
+}
