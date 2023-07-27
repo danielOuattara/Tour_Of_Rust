195d46e96d9523b89e25e3f59a77f06f46937790
danielOuattara <daniel.ouattara@gmx.com>
2023-07-27 23:04:20 +0200

    04 Generics Type: 00 intro

---
+++ src/_04_Generics_Types/_00_intro.rs
@@ -0,0 +1,17 @@
+
+/*
+
+Generics Types :
+================
+
+Generic types are incredibly important in Rust.
+They are used in the representation of :
+ - nullable values (i.e. variables which might not have a value yet),
+ - error handling,
+ - collections,
+ - and more!
+
+ In this section we will be learning about the foundational generic
+ types you will likely be using all the time.
+*/
+
