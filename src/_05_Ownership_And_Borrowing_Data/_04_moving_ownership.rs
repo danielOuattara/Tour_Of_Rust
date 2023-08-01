35920b2bdab24e18d64bdbffb6448fd874bd1fee
danielOuattara <daniel.ouattara@gmx.com>
2023-08-01 22:42:32 +0200

    05 OwnerShip: 04 moving ownership updated

--- src/_05_Ownership_And_Borrowing_Data/_04_moving_ownership.rs
+++ src/_05_Ownership_And_Borrowing_Data/_04_moving_ownership.rs
@@ -1,16 +1,16 @@
-/* 
+/*
 Moving Ownership
 =================
 
-When an owner is passed as an argument to a function, 
+When an owner is passed as an argument to a function,
 ownership is moved to the function parameter.
 
-After a move the variable in the original function can 
+After a move the variable in the original function can
 no longer be used.
 
 Memory details:
 
-- During a move the stack memory of the owners value is 
+- During a move the stack memory of the owners value is
   copied to the function call's parameter stack memory.
 
 */
@@ -29,11 +29,11 @@ fn do_something(f: Foo) {
 fn main() {
     // foo is created.
     let foo = Foo { x: 42 };
-    println!("{}", foo.x); 
-    
+    println!("{}", foo.x);
+
     // foo is moved to do_something() and handle there
     do_something(foo);
-    
+
     // foo can no longer be used
     // println!("{}", foo.x) // ERROR
 }
