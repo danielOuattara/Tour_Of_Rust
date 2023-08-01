cef9a650a66e5c1f2a6e838bd0d1028f23736314
danielOuattara <daniel.ouattara@gmx.com>
2023-08-01 22:40:58 +0200

    05 OwnerShip: 01 ownership updated

--- src/_05_Ownership_And_Borrowing_Data/_01_ownership.rs
+++ src/_05_Ownership_And_Borrowing_Data/_01_ownership.rs
@@ -1,13 +1,12 @@
-/* 
+/*
 Ownership
 ==========
 
-Instantiating a type and binding it to a variable name 
-creates a memory resource that the Rust compiler will 
-validate through its whole lifetime. 
+Instantiating a type and binding it to a variable name
+creates a memory resource that the Rust compiler will
+validate through its whole lifetime.
 
 The bound variable is called the resource's owner. */
-
 
 struct Foo {
     x: i32,
@@ -15,10 +14,10 @@ struct Foo {
 
 fn main() {
     // We instantiate structs and bind to variable foo
-    // to create memory resources
+    // to create memory resource
     let foo = Foo { x: 42 };
-    
+
     // foo is the owner
 
-    println!("{}",foo.x );
+    println!("{}", foo.x);
 }
