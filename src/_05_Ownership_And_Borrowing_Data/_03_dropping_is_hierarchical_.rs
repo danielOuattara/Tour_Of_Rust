1c7af47b66eebe0d391bc5514cd9db6287dc39c0
danielOuattara <daniel.ouattara@gmx.com>
2023-08-01 22:42:12 +0200

    05 OwnerShip: 03 dropping hierarchical updated

--- src/_05_Ownership_And_Borrowing_Data/_03_dropping_is_hierarchical_.rs
+++ src/_05_Ownership_And_Borrowing_Data/_03_dropping_is_hierarchical_.rs
@@ -1,20 +1,19 @@
-/* 
+/*
 Dropping is Hierarchical
 =========================
 
-When a struct is dropped, the struct itself is dropped 
-first, then its children are dropped individually, and 
+When a struct is dropped, the struct itself is dropped
+first, then its children are dropped individually, and
 so on.
 
 Memory Details:
 
-- By automatically freeing memory Rust helps ensure that 
+- By automatically freeing memory Rust helps ensure that
   there are fewer memory leaks.
 
 - Memory resources can only be dropped once.
 
  */
-
 
 struct Bar {
     x: i32,
